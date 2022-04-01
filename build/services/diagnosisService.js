28c1da7819fabd03911c5be7141e17e48eecae6c
mattmuroya <matt.muroya@gmail.com>
2022-03-31 21:18:41 -1000

    removed build folder: `git rm -r --cached build`

--- build/services/diagnosisService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const diagnoses_1 = require("../data/diagnoses");
-const getDiagnoses = () => {
-    return diagnoses_1.diagnoses;
-};
-exports.default = {
-    getDiagnoses
-};
